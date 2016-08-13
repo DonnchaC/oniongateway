66c8a4c9394879325616e54da06a48491fbc219d
Boris Nagaev <bnagaev@gmail.com>
2016-08-13 22:51:09 +0300

    check_entry_proxy -config checker.yaml

--- check_entry_proxy/main.go
+++ check_entry_proxy/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
+
+	"gopkg.in/yaml.v2"
 )
 
 var (
@@ -22,18 +25,37 @@ var (
 		80,
 		"HTTP port redirecting to entry proxy",
 	)
+	config = flag.String(
+		"config",
+		"",
+		"config file with rules in YAML format",
+	)
 )
 
 func main() {
 	flag.Parse()
-	checker := &Checker{
-		// TODO: move to flag
-		Rules: []Rule{
-			{"https://www.pasta.cf/mind-take-boyfriend/raw", "entry_proxy"},
-		},
-		RedirectRules: []string{
-			"http://example.com/foo",
-		},
+	var checker Checker
+	if *config != "" {
+		configData, err := ioutil.ReadFile(*config)
+		if err != nil {
+			fmt.Printf("Error reading config %s: %s\n", *config, err)
+			os.Exit(1)
+		}
+		err = yaml.Unmarshal(configData, &checker)
+		if err != nil {
+			fmt.Printf("Error parsing config %s: %s\n", *config, err)
+			os.Exit(1)
+		}
+	} else {
+		// default values
+		checker = Checker{
+			Rules: []Rule{
+				{"https://www.pasta.cf/mind-take-boyfriend/raw", "entry_proxy"},
+			},
+			RedirectRules: []string{
+				"http://example.com/foo",
+			},
+		}
 	}
 	err := checker.CheckHost(*host, *proxyPort, *redirectPort)
 	if err == nil {
