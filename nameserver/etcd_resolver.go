ea486d96ebc46202c4c85a55add67f0efa12fffb
Boris Nagaev <bnagaev@gmail.com>
2016-08-21 16:06:06 +0300

    nameserver: add etcd_resolver

---
+++ nameserver/etcd_resolver.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"path"
+	"time"
+
+	"github.com/coreos/etcd/clientv3"
+	"github.com/miekg/dns"
+	"golang.org/x/net/context"
+)
+
+// EtcdResolver resolves DNS requests from etcd
+type EtcdResolver struct {
+	Client  *clientv3.Client
+	Timeout time.Duration
+}
+
+func (r *EtcdResolver) Resolve(
+	domain string,
+	qtype, qclass uint16,
+) (
+	string,
+	error,
+) {
+	kv := clientv3.NewKV(r.Client)
+	ctx, cancel := context.WithTimeout(context.Background(), r.Timeout)
+	defer cancel()
+	if qtype == dns.TypeA || qtype == dns.TypeAAAA {
+		// TODO: watch on updates of the lists
+		var keyPrefix string
+		if qtype == dns.TypeA {
+			keyPrefix = "/ipv4/"
+		} else if qtype == dns.TypeAAAA {
+			keyPrefix = "/ipv6/"
+		}
+		resp, err := kv.Get(ctx, keyPrefix, clientv3.WithPrefix())
+		if err != nil {
+			return "", fmt.Errorf("etcd GET error qtype=%d: %s", qtype, err)
+		}
+		n := len(resp.Kvs)
+		if n == 0 {
+			return "", fmt.Errorf("No proxies for question of type %d", qtype)
+		}
+		i := rand.Intn(n)
+		key := string(resp.Kvs[i].Key)
+		address := path.Base(key)
+		return address, nil
+	} else if qtype == dns.TypeTXT {
+		key := fmt.Sprintf("/domain2onion/%s", domain)
+		resp, err := kv.Get(ctx, key)
+		if err != nil {
+			return "", fmt.Errorf("etcd GET error qtype=%d: %s", qtype, err)
+		}
+		if len(resp.Kvs) == 0 {
+			return "", fmt.Errorf("TXT request of unknown domain: %q", domain)
+		}
+		onion := string(resp.Kvs[0].Value)
+		txt := fmt.Sprintf("onion=%s", onion)
+		return txt, nil
+	} else {
+		return "", fmt.Errorf("Unknown question type: %d", qtype)
+	}
+}
---
+++ nameserver/etcd_resolver_test.go
@@ -0,0 +1,161 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/url"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/coreos/etcd/clientv3"
+	"github.com/coreos/etcd/embed"
+	"github.com/miekg/dns"
+	"github.com/phayes/freeport"
+	"golang.org/x/net/context"
+)
+
+const timeout = 5 * time.Second
+
+func makeEtcdServer() (
+	server *embed.Etcd,
+	client *clientv3.Client,
+	closer func(),
+	err error,
+) {
+	// choose ports and data directory
+	peerPort := freeport.GetPort()
+	clientPort := freeport.GetPort()
+	peerUrlStr := fmt.Sprintf("http://127.0.0.1:%d", peerPort)
+	peerUrl, err := url.Parse(peerUrlStr)
+	if err != nil {
+		err = fmt.Errorf("Failed to parse peer URL %q: %s", peerUrlStr, err)
+		return
+	}
+	clientUrlStr := fmt.Sprintf("http://127.0.0.1:%d", clientPort)
+	clientUrl, err := url.Parse(clientUrlStr)
+	if err != nil {
+		err = fmt.Errorf("Failed to parse client URL %q: %s", clientUrlStr, err)
+		return
+	}
+	tmpDir, err := ioutil.TempDir("", "oniongateway-nameserver-etcd-test")
+	if err != nil {
+		err = fmt.Errorf("Failed to create temp dir: %s", err)
+		return
+	}
+	// create server
+	cfg := embed.NewConfig()
+	cfg.Dir = tmpDir
+	cfg.APUrls = []url.URL{*peerUrl}
+	cfg.LPUrls = []url.URL{*peerUrl}
+	cfg.ACUrls = []url.URL{*clientUrl}
+	cfg.LCUrls = []url.URL{*clientUrl}
+	cfg.InitialCluster = cfg.InitialClusterFromName(cfg.Name)
+	server, err = embed.StartEtcd(cfg)
+	if err != nil {
+		err = fmt.Errorf("Failed to start etcd server: %s", err)
+		os.RemoveAll(tmpDir)
+		return
+	}
+	// create client
+	client, err = clientv3.New(clientv3.Config{
+		Endpoints:   []string{fmt.Sprintf("127.0.0.1:%d", clientPort)},
+		DialTimeout: timeout,
+	})
+	if err != nil {
+		os.RemoveAll(tmpDir)
+		err = fmt.Errorf("Failed to create etcd client: %s", err)
+		return
+	}
+	closer = func() {
+		client.Close()
+		server.Close()
+		os.RemoveAll(tmpDir)
+	}
+	return
+}
+
+func populateDatabase(client *clientv3.Client) error {
+	kv := clientv3.NewKV(client)
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+	_, err := kv.Put(ctx, "/ipv4/127.0.0.1", "not used")
+	if err != nil {
+		return fmt.Errorf("Failed to put key /ipv4/127.0.0.1: %s", err)
+	}
+	_, err = kv.Put(ctx, "/ipv6/::1", "not used")
+	if err != nil {
+		return fmt.Errorf("Failed to put key /ipv6/::1: %s", err)
+	}
+	_, err = kv.Put(ctx, "/domain2onion/pasta.cf.", "pastagdsp33j7aoq.onion")
+	if err != nil {
+		return fmt.Errorf("Failed to put key /domain2onion/pasta.cf.: %s", err)
+	}
+	return nil
+}
+
+func TestEtcdResolver(t *testing.T) {
+	_, client, closer, err := makeEtcdServer()
+	if err != nil {
+		t.Fatalf("Failed to create etcd server and client: %s", err)
+	}
+	defer closer()
+	if err = populateDatabase(client); err != nil {
+		t.Fatalf("Failed to populate etcd with example data: %s", err)
+	}
+	resolver := &EtcdResolver{
+		Client:  client,
+		Timeout: timeout,
+	}
+	// IPv4
+	address, err := resolver.Resolve("example.com.", dns.TypeA, dns.ClassINET)
+	if err != nil {
+		t.Fatalf("Failed to resolve %q to IPv4", "example.com.")
+	}
+	if address != "127.0.0.1" {
+		t.Fatalf("Wrong IPv4 address was returned: %q", address)
+	}
+	// IPv6
+	address, err = resolver.Resolve("example.com.", dns.TypeAAAA, dns.ClassINET)
+	if err != nil {
+		t.Fatalf("Failed to resolve %q to IPv6", "example.com.")
+	}
+	if address != "::1" {
+		t.Fatalf("Wrong IPv6 address was returned: %q", address)
+	}
+	// TXT
+	txt, err := resolver.Resolve("pasta.cf.", dns.TypeTXT, dns.ClassINET)
+	if err != nil {
+		t.Fatalf("Failed to get TXT record for %q", "pasta.cf.")
+	}
+	if txt != "onion=pastagdsp33j7aoq.onion" {
+		t.Fatalf("Wrong TXT response was returned: %q", txt)
+	}
+}
+
+func TestEmptyResolverAbsent(t *testing.T) {
+	_, client, closer, err := makeEtcdServer()
+	if err != nil {
+		t.Fatalf("Failed to create etcd server and client: %s", err)
+	}
+	defer closer()
+	resolver := &EtcdResolver{
+		Client:  client,
+		Timeout: timeout,
+	}
+	// IPv4
+	address, err := resolver.Resolve("example.com.", dns.TypeA, dns.ClassINET)
+	if err == nil {
+		t.Fatalf("IPv4 request expected to fail returned %q", address)
+	}
+	// IPv6
+	address, err = resolver.Resolve("example.com.", dns.TypeAAAA, dns.ClassINET)
+	if err == nil {
+		t.Fatalf("IPv6 request expected to fail returned %q", address)
+	}
+	// TXT
+	txt, err := resolver.Resolve("pasta.cf.", dns.TypeTXT, dns.ClassINET)
+	if err == nil {
+		t.Fatalf("TXT request expected to fail returned %q", txt)
+	}
+}
