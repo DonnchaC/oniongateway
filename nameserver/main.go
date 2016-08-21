b08fceafe696a0a5aec85594ef57626808f1a27a
Boris Nagaev <bnagaev@gmail.com>
2016-08-21 16:06:27 +0300

    nameserver uses fixed- and etcd resolver

--- nameserver/main.go
+++ nameserver/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"strings"
+	"time"
 
+	"github.com/coreos/etcd/clientv3"
 	"github.com/miekg/dns"
+	"gopkg.in/yaml.v2"
 )
 
 type Resolver interface {
@@ -49,24 +55,70 @@ func (h *dnsHandler) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
 	}
 }
 
+var (
+	fixedConfig = flag.String(
+		"fixed-config",
+		"",
+		"config file with rules in YAML format",
+	)
+	etcdEndpoints = flag.String(
+		"etcd-endpoints",
+		"",
+		"comma-separated list of etcd endpoints",
+	)
+	etcdTimeout = flag.Duration(
+		"etcd-timeout",
+		5*time.Second,
+		"Timeout used in etcd client",
+	)
+	listenAddr = flag.String(
+		"listen-addr",
+		":53",
+		"Address of DNS server to create",
+	)
+	listenNet = flag.String(
+		"listen-net",
+		"udp",
+		"Network of DNS server to create",
+	)
+)
+
 func main() {
+	flag.Parse()
+	var handler dnsHandler
+	if *fixedConfig != "" && *etcdEndpoints != "" {
+		log.Fatalf("Provide one of -fixed-config and -etcd-endpoints")
+	} else if *fixedConfig != "" {
+		configData, err := ioutil.ReadFile(*fixedConfig)
+		if err != nil {
+			log.Fatalf("Error reading config %s: %s\n", *fixedConfig, err)
+		}
+		var fixedResolver FixedResolver
+		err = yaml.Unmarshal(configData, &fixedResolver)
+		if err != nil {
+			log.Fatalf("Error parsing config %s: %s\n", *fixedConfig, err)
+		}
+		handler.resolver = &fixedResolver
+	} else if *etcdEndpoints != "" {
+		endpoints := strings.Split(*etcdEndpoints, ",")
+		client, err := clientv3.New(clientv3.Config{
+			Endpoints:   endpoints,
+			DialTimeout: *etcdTimeout,
+		})
+		if err != nil {
+			log.Fatalf("Error creating etcd client: %s", err)
+		}
+		handler.resolver = &EtcdResolver{
+			Client:  client,
+			Timeout: *etcdTimeout,
+		}
+	} else {
+		log.Fatalf("Provide one of -fixed-config and -etcd-endpoints")
+	}
 	server := &dns.Server{
-		Addr: ":4253",
-		Net:  "udp",
-		Handler: &dnsHandler{
-			resolver: &FixedResolver{
-				IPv4Proxies: []string{
-					"127.0.0.1",
-					"127.0.0.2",
-				},
-				IPv6Proxies: []string{
-					"::1",
-				},
-				Domain2Onion: map[string]string{
-					"pasta.cf.": "pastagdsp33j7aoq.onion",
-				},
-			},
-		},
+		Addr:    *listenAddr,
+		Net:     *listenNet,
+		Handler: &handler,
 	}
 	err := server.ListenAndServe()
 	defer server.Shutdown()
